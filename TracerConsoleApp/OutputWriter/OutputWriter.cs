416f0c4242727f6e03eb3a8e94ea130607d8bc54
den25570 <den255700@gmail.com>
2020-09-06 21:09:12 +0300

    Add serialization & outpot writer classes
    - add console app project
    - link console app & library
    - add serialization interface
    - add serialization JSON
    - add serialization XML
    - add OutputWriter iterface
    - add OutputWriter realization

---
+++ TracerConsoleApp/OutputWriter/OutputWriter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TracerConsoleApp
+{
+    namespace OutputWriter {
+        class OutputWriter : IOutputWriter
+        {
+            public void Write(string toWrite, StreamWriter writer = null)
+            {
+                if (writer != null)
+                {
+                    Console.SetOut(writer);
+                }
+                Console.WriteLine(toWrite);
+            }
+        }
+    }
+}
---
+++ TracerConsoleApp/Serializer/SerializerXML.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.ComponentModel;
+using System.Globalization;
+using Tracer;
+using TracerConsoleApp.Serializer;
+using System.Xml.Serialization;
+
+namespace TracerConsoleApp
+{
+    namespace Serializer
+    {
+        class SerializerXML : ISerializer
+        {
+            public string Serialize(string path, TraceResult traceResult)
+            {
+                string output;
+                XmlSerializer formatter = new XmlSerializer(typeof(TraceResult));
+
+                using (StringWriter textWriter = new StringWriter())
+                {
+                    formatter.Serialize(textWriter, traceResult);
+                    output = textWriter.ToString();
+                }
+
+                return output;
+            }
+
+        }
+    }
+}
+
